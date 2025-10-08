2cf9d4da85dc99440d087c21e81b62d1aebba828
bpetel <baptiste.petel@protonmail.com>
2025-10-08 14:10:19 +0200

    Add Retrofit2 dependencies

--- app/src/main/java/com/bpetel/newsandroidapp/MainActivity.kt
+++ app/src/main/java/com/bpetel/newsandroidapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.bpetel.newsandroidapp.ui.theme.NewsAndroidAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
